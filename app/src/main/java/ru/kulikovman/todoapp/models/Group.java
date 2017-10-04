3e247b635fc5c4f2b7382ac642174d793f2b5254
Maksim Kulikov <kulikovman@gmail.com>
2017-10-04 19:42:29 +0500

    В модель группы добавлено поле описания

--- app/src/main/java/ru/kulikovman/todoapp/models/Group.java
+++ app/src/main/java/ru/kulikovman/todoapp/models/Group.java
@@ -3,7 +3,14 @@ package ru.kulikovman.todoapp.models;
 
 public class Group {
     private String mName;
+    private String mDescription;
     private int mColorId;
+
+    public Group(String name, String description, int colorId) {
+        mName = name;
+        mDescription = description;
+        mColorId = colorId;
+    }
 
     public Group(String name, int colorId) {
         mName = name;
@@ -24,5 +31,13 @@ public class Group {
 
     public void setColorId(int colorId) {
         mColorId = colorId;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
     }
 }
