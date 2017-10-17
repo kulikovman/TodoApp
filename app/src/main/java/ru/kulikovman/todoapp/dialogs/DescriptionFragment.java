90ede5bc5db2c7c599922787567f6bb465a71136
Maksim Kulikov <kulikovman@gmail.com>
2017-10-17 18:08:06 +0500

    Завершен диалог создания описания группы

--- app/src/main/java/ru/kulikovman/todoapp/dialogs/DescriptionFragment.java
+++ app/src/main/java/ru/kulikovman/todoapp/dialogs/DescriptionFragment.java
@@ -8,6 +8,7 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -20,20 +21,34 @@ public class DescriptionFragment extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Это нужно для привязки к диалогу вью из макета
         LayoutInflater inflater = getActivity().getLayoutInflater();
+        View dialogDescription = inflater.inflate(R.layout.dialog_description, null);
 
+        // Инициализируем вью элементы
+        final EditText dialogInputText = (EditText) dialogDescription.findViewById(R.id.dialog_input_text);
+        final TextView descriptionState = (TextView) getActivity().findViewById(R.id.description_state);
 
+        // Получаем строки для последующего сравнения
+        String currentDescription = descriptionState.getText().toString();
+        String withoutDescription = getString(R.string.without_description);
+
+        // Передаем текст текущего описания в поле диалога
+        if (!currentDescription.equals(withoutDescription)) {
+            dialogInputText.setText(currentDescription);
+        }
+
+        // Формируем диалог при помощи конструктора
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(R.string.description_title)
-                .setView(inflater.inflate(R.layout.dialog_description, null))
+                .setView(dialogDescription)
                 .setPositiveButton(R.string.save_button, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        EditText textDescription = (EditText) getActivity().findViewById(R.id.text_description);
-                        String temp = textDescription.getText().toString();
+                        // Получаем введенный текст и очищаем от лишних пробелов
+                        String updatedDescription = dialogInputText.getText().toString().trim();
 
-                        if (!temp.equals("")) {
-                            TextView descriptionState = (TextView) getActivity().findViewById(R.id.description_state);
-                            descriptionState.setText(temp);
+                        // Сохраняем введенный текст в поле с описанием группы
+                        if (updatedDescription.length() > 0) {
+                            descriptionState.setText(updatedDescription);
                         }
                     }
                 });
