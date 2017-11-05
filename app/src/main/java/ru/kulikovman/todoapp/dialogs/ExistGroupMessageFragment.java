af039c3d3b93b1cc6959edbb61d9911a31e3bb46
Maksim Kulikov <kulikovman@gmail.com>
2017-11-05 18:50:20 +0500

    Создан диалог с сообщением

--- app/src/main/java/ru/kulikovman/todoapp/dialogs/ExistGroupMessageFragment.java
+++ app/src/main/java/ru/kulikovman/todoapp/dialogs/ExistGroupMessageFragment.java
@@ -1,5 +1,30 @@
 package ru.kulikovman.todoapp.dialogs;
 
 
-public class ExistGroupMessageFragment {
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+import ru.kulikovman.todoapp.R;
+
+public class ExistGroupMessageFragment extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.dialog_exists_group_title)
+                .setMessage(R.string.dialog_exists_group_message)
+                .setPositiveButton(R.string.dialog_exists_group_close, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Ничего не делаем при нажатии
+                    }
+                });
+
+        return builder.create();
+    }
 }
