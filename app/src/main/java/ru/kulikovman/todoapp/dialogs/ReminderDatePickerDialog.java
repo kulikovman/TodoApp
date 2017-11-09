e212a5100d561b3f0529a7f1e6222620037b82a1
Maksim Kulikov <kulikovman@gmail.com>
2017-11-09 15:33:05 +0500

    Почти готов диалог выбора даты напоминания

---
+++ app/src/main/java/ru/kulikovman/todoapp/dialogs/ReminderDatePickerDialog.java
@@ -0,0 +1,41 @@
+package ru.kulikovman.todoapp.dialogs;
+
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+import ru.kulikovman.todoapp.R;
+
+public class ReminderDatePickerDialog extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        Calendar calendar = new GregorianCalendar(view.getYear(), view.getMonth(), view.getDayOfMonth());
+        DateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy", Locale.getDefault());
+        String date = dateFormat.format(calendar.getTime());
+
+        TextView dateField = (TextView) getActivity().findViewById(R.id.date_state);
+        dateField.setText(date);
+    }
+}
---
+++ app/src/main/java/ru/kulikovman/todoapp/dialogs/ReminderDialog.java
@@ -0,0 +1,91 @@
+package ru.kulikovman.todoapp.dialogs;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import ru.kulikovman.todoapp.R;
+
+
+public class ReminderDialog extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        String onTheDay = getString(R.string.reminder_on_the_day);
+        String dayBefore = getString(R.string.reminder_day_before);
+        String weekBefore = getString(R.string.reminder_week_before);
+        String monthBefore = getString(R.string.reminder_month_before);
+        String pickDate = getString(R.string.reminder_pick_date);
+        String not = getString(R.string.reminder_not);
+
+        final String reminder[] = {onTheDay, dayBefore, weekBefore, monthBefore, pickDate, not};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.reminder_title)
+                .setItems(reminder, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d("myLog", String.valueOf(which));
+
+                        // Инициализируем нужные вью
+                        TextView dateState = (TextView) getActivity().findViewById(R.id.date_state);
+                        TextView reminderState = (TextView) getActivity().findViewById(R.id.reminder_state);
+
+                        // Получаем дату задачи
+                        String selectedDate = dateState.getText().toString();
+
+                        // Записываем выбранную дату в поле
+                        if (!selectedDate.equals(getString(R.string.date_without))) {
+                            // Если у задачи нет даты, то показываем сообщение
+                            // ...
+                        } else {
+                            // Если дата есть, то преобразуем ее из текста в календарь
+                            Calendar calendar = Calendar.getInstance();
+                            DateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy", Locale.getDefault());
+                            try {
+                                calendar.setTime(dateFormat.parse(selectedDate));
+                            } catch (ParseException ignored) {
+                            }
+
+                            // Записываем в поле выбранную дату
+                            switch (which) {
+                                case 0:
+                                    reminderState.setText(selectedDate);
+                                    break;
+                                case 1:
+                                    calendar.add(Calendar.DAY_OF_YEAR, -1);
+                                    reminderState.setText(dateFormat.format(calendar.getTime()));
+                                    break;
+                                case 2:
+                                    calendar.add(Calendar.WEEK_OF_YEAR, -1);
+                                    reminderState.setText(dateFormat.format(calendar.getTime()));
+                                    break;
+                                case 3:
+                                    calendar.add(Calendar.MONTH, -1);
+                                    reminderState.setText(dateFormat.format(calendar.getTime()));
+                                    break;
+                                case 4:
+                                    // Запуск диалога выбора даты напоминания
+                                    DialogFragment datePickerFragment = new DatePickerDialog();
+                                    datePickerFragment.show(getFragmentManager(), "datePicker");
+                                    break;
+                                case 5:
+                                    reminderState.setText(R.string.reminder_not);
+                                    break;
+                            }
+                        }
+                    }
+                });
+
+        return builder.create();
+    }
+}
