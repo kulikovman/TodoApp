b0b1ca06a8fec8c02cb96755fabe88b7ec230cb1
Maksim Kulikov <kulikovman@gmail.com>
2017-11-09 15:38:24 +0500

    Конвертация строковой даты в карендарь

--- app/src/main/java/ru/kulikovman/todoapp/Helper.java
+++ app/src/main/java/ru/kulikovman/todoapp/Helper.java
@@ -1,5 +1,22 @@
 package ru.kulikovman.todoapp;
 
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class Helper {
+    public static Calendar convertTextDateToCalendar(String sourceDate) {
+        Calendar calendar = Calendar.getInstance();
+        DateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy", Locale.getDefault());
+        try {
+            calendar.setTime(dateFormat.parse(sourceDate));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return calendar;
+    }
 }
